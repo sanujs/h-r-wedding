0cd8c66bce6b0b4a2acc55019e13bbbc03750d7d
Sanuj Syal <sanujsyal@gmail.com>
2024-08-22 17:23:04 -0400

    Additional timeline elements and new font

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,19 +1,30 @@
+import dhol from "../assets/dhol1.jpg";
+import ganesh from "../assets/ganesh.jpg";
+import garland from "../assets/garland.png";
 const Timeline = () => {
 
   return (
     <div className="timeline">
       <div className="event">
         <div className="event-text">
-          <h2>1. Baratt</h2>
-          <img src="src/assets/dhol1.jpg"/>
+          <h2>Baraat</h2>
+          <img src={dhol}/>
         </div>
         <p>Rohith, his family, and friends arrive at the venue, accomponied by music, a beating dhol (drum), and their best dance moves.</p>
       </div>
       <div className="event">
         <div className="event-text">
-          <h2>2. Ganesh Pooja</h2>
-          <p>The ceremony begins with a prayer to Lord Ganesha, an eternal symbol of peace, happiness, and friendship.</p>
+          <h2 id="ganesh">Ganesh Pooja</h2>
+          <img src={ganesh}/>
         </div>
+        <p>The ceremony begins with a prayer to Lord Ganesha, an eternal symbol of peace, happiness, and friendship.</p>
+      </div>
+      <div className="event">
+        <div className="event-text">
+          <h2>Jai Mala</h2>
+          <img src={garland}/>
+        </div>
+        <p>Hindu ceremonies start with the groom and bride's parents. Now that Rohith has been accepted by the family, it's time to bring Harena to the mandap. Rohith's chawl is tied to Varsha's dupatta (scarf), representing acceptance and a new bond.</p>
       </div>
     </div>
   );
